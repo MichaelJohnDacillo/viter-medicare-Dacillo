4efac5eaed98706f580be4386cb96eefe1681013
unknown <aldrin.brinas@frontlinebusiness.com.ph>
2025-07-11 14:35:15 +0800

    some changes

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-import { FaClinicMedical } from 'react-icons/fa';
+import { FaClinicMedical, FaTimes } from 'react-icons/fa';
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <>
         {/* Navigation */}
@@ -19,25 +24,31 @@ const Header = () => {
                         <a href="#emergency" className='text-blue-800 font-medium hover:text-blue-600'>Emergency</a>
                         <a href="#pharmacy" className='text-blue-800 font-medium hover:text-blue-600'>Pharmacy</a>
                     </div>
-                    <button className="md:hidden text-gray-600">
-                        <svg
-                            className="w-6 h-6"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                            >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M4 6h16M4 12h16M4 18h16"
-                            />
-                        </svg>
+                    <button
+                    onClick={toggleMenu}
+                    className="md:hidden text-xl text-gray-600 focus:outline-none">
+                        {isMenuOpen ? <FaTimes /> : "☰"}
                     </button>
                 </div>
             </div>
         </nav>
+
+        {/* Mobile Menu */}
+      <div
+        className={`fixed inset-0 bg-white z-40 flex flex-col items-center justify-center transition-all duration-300 ease-in-out ${
+          isMenuOpen
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 -translate-y-full pointer-events-none"
+        }`}
+      >
+        <div className="flex flex-col items-center space-y-8 text-white text-2xl">
+          <a href="#home" className='text-blue-800 font-medium hover:text-blue-600'>Home</a>
+            <a href="#services" className='text-blue-800 font-medium hover:text-blue-600' onClick={() => setIsMenuOpen(false)}>Services</a>
+            <a href="#doctors" className='text-blue-800 font-medium hover:text-blue-600' onClick={() => setIsMenuOpen(false)}>Doctors</a>
+            <a href="#emergency" className='text-blue-800 font-medium hover:text-blue-600' onClick={() => setIsMenuOpen(false)}>Emergency</a>
+            <a href="#pharmacy" className='text-blue-800 font-medium hover:text-blue-600' onClick={() => setIsMenuOpen(false)}>Pharmacy</a>
+      </div>
+    </div>
     </>
   );
 };
